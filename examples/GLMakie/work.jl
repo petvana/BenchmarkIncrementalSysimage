50fe1b5b60c1d24caaf4612608ae127ad293dd04
Petr Vana <petvana@centrum.cz>
2022-07-11 08:42:36 +0200

    Produce pretty tables, output image for GLMakie

--- examples/GLMakie/work.jl
+++ examples/GLMakie/work.jl
@@ -4,4 +4,6 @@ xs = LinRange(0, 10, 100)
 ys = LinRange(0, 15, 100)
 zs = [cos(x) * sin(y) for x in xs, y in ys]
 
-surface(xs, ys, zs, axis=(type=Axis3,))
+fig = surface(xs, ys, zs, axis=(type=Axis3,))
+
+save("normal.png", fig)
--- run.jl
+++ run.jl
@@ -1,3 +1,6 @@
+using DataFrames
+using PrettyTables
+
 julia_dir = "julia"
 
 # Download the specific branch of Julia, here 1.7.3 with incremental compilation of sysimage
@@ -5,33 +8,27 @@ if !ispath(julia_dir)
     run(`git clone --depth 1 --branch pv/kf/fastsysimg-1.7-use-precompile https://github.com/petvana/julia`)
 end
 
-if !ispath("$julia_dir/julia")
+#if !ispath("$julia_dir/julia")
     #run(`make -j4 -C julia`)
-end
+#end
+run(`make -j4 -C julia`)
 
 julia_cmd = "julia/usr/bin/julia"
 julia_sysimage = "julia/usr/lib/julia/sys.so"
 
 chained_dir = "chained"
 
-function measure(file, chained = false)
+function measure(file, chained = false, precompiles = false)
     t = time()
     image = chained ? "-J chained/chained.so" : ""
     work = isnothing(file) ? "-e \"\"" : "$file"
-    txt = "$julia_cmd $image $work"
+    prec = precompiles ? " --trace-compile=statements.txt" : ""
+    txt = "$julia_cmd $image $prec $work"
     run(Cmd(String.(split(txt))))
     return time() - t
 end
 
-function compile_chained(source_dir)
-    t_run = measure("$source_dir/load.jl")
-    println("Runned normal in $t_run")
-    t_run = measure("$source_dir/load.jl")
-    println("Runned normal in $t_run")
-    t_run = measure("$source_dir/work.jl")
-    println("Runned work in $t_run")
-
-    # Compile the chained sysimage
+function compile(file)
     t_compile = time()
     mkpath(chained_dir)
     cd(chained_dir)
@@ -41,15 +38,43 @@ function compile_chained(source_dir)
     run(`mv text.o text-old.o`)
     run(`llvm-objcopy --remove-section .data.jl.sysimg_link text-old.o`) # rm the link between the native code and 
     cd("..")
-    source_txt = "Base.__init_build(); include(\"$source_dir/load.jl\");"
+
+    source_txt = """
+
+Base.__init_build(); 
+module PrecompileStagingArea;
+include(\"$file\");
+end;
+@ccall jl_precompiles_for_sysimage(1::Cuchar)::Cvoid;
+include("precompile.jl")
+    """
+
     run(`$julia_cmd --sysimage-native-code=chained --sysimage=julia/usr/lib/julia/sys.so --output-o chained/chained.o.a -e $source_txt`)
-    t_compile = time() - t_compile
-    println("Compiled in $t_compile")
 
     cd(chained_dir)
     run(`ar x chained.o.a`) # Extract new sysimage files
     run(`ld --allow-multiple-definition -shared -o chained.so text.o data.o text-old.o`)
     cd("..")
+
+    return time() - t_compile
+end
+
+df = DataFrame()
+
+function compile_chained(source_dir)
+    t_nempty = measure(nothing)
+    println("Run empty in $t_nempty")
+    t_nload = measure("$source_dir/load.jl")
+    println("Runned normal in $t_nload")
+    t_nload = measure("$source_dir/load.jl")
+    println("Runned normal in $t_nload")
+    t_nwork = measure("$source_dir/work.jl")
+    println("Runned work in $t_nwork")
+
+    # Compile the chained sysimage
+    run(`rm -f statements.txt`)
+    t_compile = compile("$source_dir/load.jl")
+    println("Compiled in $t_compile")
 
     t_empty = measure(nothing, true)
     println("Run empty in $t_empty")
@@ -57,23 +82,53 @@ function compile_chained(source_dir)
     t_load = measure("$source_dir/load.jl", true)
     println("Run load in $t_load")
 
-    t_work = measure("$source_dir/work.jl", true)
+    t_work = measure("$source_dir/work.jl", true, true)
     println("Run work in $t_work")
+
+    try
+        # Compile the chained sysimage
+        t_compile = compile("$source_dir/load.jl")
+        println("Compiled in $t_compile")
+    
+        t_empty = measure(nothing, true)
+        println("Run empty in $t_empty")
+    
+        t_load = measure("$source_dir/load.jl", true)
+        println("Run load in $t_load")
+    
+        t_work = measure("$source_dir/work.jl", true)
+        println("Run work in $t_work")
+    catch
+    end
+
+    push!(df, (
+        library = splitpath(source_dir)[end],
+        N_empty = t_nempty,
+        N_load = t_nload,
+        N_work = t_nwork,
+        Compile = t_compile,
+        S_empty = t_empty, 
+        S_load = t_load,
+        S_work = t_work,
+    ))
 end
 
 examples = Set(readdir("examples"))
 delete!(examples, "GLMakie")
 
-examples = ["OhMyREPL", "DataFrames", "GLMakie"]
-#examples = ["Plots"]
-#examples = ["GLMakie"]
+#examples = ["OhMyREPL"]
+examples = ["OhMyREPL", "DataFrames", "Plots", "GLMakie"]
 
 for lib in examples# ["DataFrames", "OhMyREPL"]
     println(" --- $lib --- ")
     compile_chained(joinpath("examples/", lib))
+    #break
 end
 
+@show df
+df[:, 2:end] = round.(df[:, 2:end], digits = 2)
 
+pretty_table(df, tf = tf_markdown)
 
 
     #=
