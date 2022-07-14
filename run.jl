e4764f0fecfb6e3c8336a0595903c05ba80a7b3a
Petr Vana <petvana@centrum.cz>
2022-07-14 21:02:45 +0200

    Use LLVM_full_jll to make the code multiplatform

--- run.jl
+++ run.jl
@@ -1,5 +1,6 @@
 using DataFrames
 using PrettyTables
+using LLVM_full_jll
 
 julia_dir = "julia"
 
@@ -9,7 +10,7 @@ if ispath(julia_local)
 else
     # Download the specific branch of Julia, here 1.7.3 with incremental compilation of sysimage
     if !ispath(julia_dir)
-        run(`git clone --depth 1 --branch pv/kf/fastsysimg-1.7-use-precompile https://github.com/petvana/julia`)
+        run(`git clone --depth 1 --branch pv/kf/fastsysimg https://github.com/petvana/julia`)
     end
 end
 
@@ -21,6 +22,12 @@ julia_cmd = "$julia_dir/usr/bin/julia"
 julia_sysimage = "$julia_dir/usr/lib/julia/sys.so"
 
 chained_dir = "chained"
+
+llvm_config = LLVM_full_jll.get_llvm_config_path()
+objcopy = replace(llvm_config, "llvm-config" => "llvm-objcopy")
+ar = replace(llvm_config, "llvm-config" => "llvm-ar")
+clang = replace(llvm_config, "llvm-config" => "clang")
+@show objcopy, ar, clang
 
 function measure(file, chained = false, precompiles = false)
     t = time()
@@ -37,10 +44,10 @@ function compile(file)
     mkpath(chained_dir)
     cd(chained_dir)
     cp("$julia_dir/usr/lib/julia/sys-o.a", "sys-o.a", force=true)
-    run(`ar x sys-o.a`)
+    run(`$ar x sys-o.a`)
     run(`rm data.o`)
     run(`mv text.o text-old.o`)
-    run(`llvm-objcopy --remove-section .data.jl.sysimg_link text-old.o`) # rm the link between the native code and 
+    run(`$objcopy --remove-section .data.jl.sysimg_link text-old.o`) # rm the link between the native code and 
     cd("..")
 
     source_txt = """
@@ -56,8 +63,8 @@ include("precompile.jl")
     run(`$julia_cmd --sysimage-native-code=chained --sysimage=$julia_dir/usr/lib/julia/sys.so --output-o chained/chained.o.a -e $source_txt`)
 
     cd(chained_dir)
-    run(`ar x chained.o.a`) # Extract new sysimage files
-    run(`ld --allow-multiple-definition -shared -o chained.so text.o data.o text-old.o`)
+    run(`$ar x chained.o.a`) # Extract new sysimage files
+    run(`$clang -shared -o chained.so text.o data.o text-old.o`) # --allow-multiple-definition 
     cd("..")
 
     return time() - t_compile
